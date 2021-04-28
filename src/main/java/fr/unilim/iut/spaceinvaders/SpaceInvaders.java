7fde5dcfb5d71000255b8ea76d90d9b098c67444
Baptiste Posseme <baptiste.posseme@etu.unilim.fr>
2021-04-28 17:30:57 +0200

    Files

--- src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
@@ -1,25 +1,64 @@
 package fr.unilim.iut.spaceinvaders;
 
+import fr.unilim.iut.spaceinvaders.utils.HorsEspaceJeuException;
+
 public class SpaceInvaders {
 
+    public static final char MARQUE_VAISSEAU = 'V';
+    public static final char MARQUE_VIDE = '.';
     int longueur;
     int hauteur;
+    private Vaisseau vaisseau;
 
     public SpaceInvaders(int longueur, int hauteur) {
         this.longueur = longueur;
         this.hauteur = hauteur;
     }
+    public void positionnerUnNouveauVaisseau(int x, int y) {
+
+        if (  !estDansEspaceJeu(x, y) )
+            throw new HorsEspaceJeuException("La position du vaisseau est en dehors de l'espace jeu");
+
+        vaisseau = new Vaisseau(x, y);
+    }
+
+    private boolean estDansEspaceJeu(int x, int y) {
+        return ((x >= 0) && (x < longueur)) && ((y >= 0) && (y < hauteur));
+    }
 
     @Override
     public String toString() {
+        return recupererEspaceJeuDansChaineASCII();
+    }
+
+    public String recupererEspaceJeuDansChaineASCII() {
         StringBuilder espaceDeJeu = new StringBuilder();
-        for (int i = 0; i < hauteur; i++) {
-            for (int j = 0; j < longueur; j++) {
-                espaceDeJeu.append('.');
+        for (int y = 0; y < hauteur; y++) {
+            for (int x = 0; x < longueur; x++) {
+
+                espaceDeJeu.append(recupererMarqueDeLaPosition(x, y));
             }
             espaceDeJeu.append('\n');
         }
         return espaceDeJeu.toString();
     }
+
+    private char recupererMarqueDeLaPosition(int x, int y) {
+        char marque;
+        if (this.aUnVaisseauQuiOccupeLaPosition(x, y))
+            marque= MARQUE_VAISSEAU;
+        else
+            marque= MARQUE_VIDE;
+        return marque;
+    }
+
+    private boolean aUnVaisseauQuiOccupeLaPosition(int y, int x) {
+        return this.aUnVaisseau() && vaisseau.occupeLaPosition(y, x);
+    }
+
+    private boolean aUnVaisseau() {
+        return vaisseau != null;
+    }
+
 
 }
--- src/test/java/fr/unilim/iut/spaceinvaders/SpaceInvadersTest.java
+++ src/test/java/fr/unilim/iut/spaceinvaders/SpaceInvadersTest.java
@@ -1,9 +1,18 @@
 package fr.unilim.iut.spaceinvaders;
+import static org.junit.Assert.fail;
+import org.junit.Before;
 
 import static org.junit.Assert.assertEquals;
+
+import fr.unilim.iut.spaceinvaders.utils.HorsEspaceJeuException;
 import org.junit.Test;
 
 public class SpaceInvadersTest {
+    private SpaceInvaders spaceinvaders;
+    @Before
+    public void initialisation() {
+        spaceinvaders = new SpaceInvaders(15, 10);
+    }
 
     @Test
     public void test_AuDebut_JeuSpaceInvaderEstVide() {
@@ -18,7 +27,64 @@ public class SpaceInvadersTest {
                 "...............\n" +
                 "...............\n" +
                 "...............\n" +
-                "...............\n" , spaceinvaders.toString());
+                "...............\n" , spaceinvaders.recupererEspaceJeuDansChaineASCII ());
+    }
+
+    @Test
+    public void test_unNouveauVaisseauEstCorrectementPositionneDansEspaceJeu() {
+        SpaceInvaders spaceinvaders = new SpaceInvaders(15, 10);
+        spaceinvaders.positionnerUnNouveauVaisseau(7,9);
+        assertEquals("" +
+                "...............\n" +
+                "...............\n" +
+                "...............\n" +
+                "...............\n" +
+                "...............\n" +
+                "...............\n" +
+                "...............\n" +
+                "...............\n" +
+                "...............\n" +
+                ".......V.......\n" , spaceinvaders.recupererEspaceJeuDansChaineASCII ());
+    }
+
+    @Test(expected = HorsEspaceJeuException.class)
+    public void test_unNouveauVaisseauEstPositionneHorsEspaceJeuTropADroite_UneExceptionEstLevee() throws Exception {
+        SpaceInvaders spaceinvaders = new SpaceInvaders(15, 10);
+        spaceinvaders.positionnerUnNouveauVaisseau(15,9);
+    }
+
+
+    @Test
+    public void test_UnNouveauVaisseauPositionneHorsEspaceJeu_DoitLeverUneException() {
+        SpaceInvaders spaceinvaders = new SpaceInvaders(15, 10);
+
+        try {
+            spaceinvaders.positionnerUnNouveauVaisseau(15,9);
+            fail("Position trop à droite : devrait déclencher une exception HorsEspaceJeuException");
+        } catch (final HorsEspaceJeuException e) {
+        }
+
+
+        try {
+            spaceinvaders.positionnerUnNouveauVaisseau(-1,9);
+            fail("Position trop à gauche : devrait déclencher une exception HorsEspaceJeuException");
+        } catch (final HorsEspaceJeuException e) {
+        }
+
+
+        try {
+            spaceinvaders.positionnerUnNouveauVaisseau(14,10);
+            fail("Position trop en bas : devrait déclencher une exception HorsEspaceJeuException");
+        } catch (final HorsEspaceJeuException e) {
+        }
+
+
+        try {
+            spaceinvaders.positionnerUnNouveauVaisseau(14,-1);
+            fail("Position trop à haut : devrait déclencher une exception HorsEspaceJeuException");
+        } catch (final HorsEspaceJeuException e) {
+        }
+
     }
 
 }
